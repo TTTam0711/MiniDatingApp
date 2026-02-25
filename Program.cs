0119d64aef539c2a7fb2e2e3212c878b42c52323
Tam Than <thanthanhtam2002@gmail.com>
2026-02-25 09:40:35 +0700

    clean repo

--- Program.cs
+++ Program.cs
@@ -2,28 +2,42 @@ using MiniDatingApp.Components;
 using MiniDatingApp.Data;
 using Microsoft.EntityFrameworkCore;
 using MiniDatingApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=app.db"));
+
 builder.Services.AddScoped<ProfileService>();
 builder.Services.AddScoped<MatchService>();
 
 var app = builder.Build();
 
+if (!app.Environment.IsDevelopment())
+{
+    app.Urls.Add("http://0.0.0.0:8080");
+}
+
+// ?? IMPORTANT: Auto migrate database on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+// ?? OPTIONAL: có th? gi? ho?c b?
 app.UseHttpsRedirection();
-
 
 app.UseAntiforgery();
 
